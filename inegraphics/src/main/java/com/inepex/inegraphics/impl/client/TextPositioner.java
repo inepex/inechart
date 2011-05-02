c31a7a031944afe14613e4ac64074b23b79a6663
miklos.suveges <miklos.suveges@inepex.com>
2011-05-02 19:52:24 +0000

    Text positioning refinements
    git-svn-id: http://inechart.googlecode.com/svn/trunk@216 8b7161a9-059b-3e57-6d5a-16b2b4bc90f4

--- inegraphics/src/main/java/com/inepex/inegraphics/impl/client/TextPositioner.java
+++ inegraphics/src/main/java/com/inepex/inegraphics/impl/client/TextPositioner.java
@@ -3,14 +3,22 @@ package com.inepex.inegraphics.impl.client;
 
 import java.util.TreeMap;
 
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.inepex.inegraphics.shared.gobjects.Text;
+import com.inepex.inegraphics.shared.gobjects.Text.BasePointXPosition;
+import com.inepex.inegraphics.shared.gobjects.Text.BasePointYPosition;
 
 public class TextPositioner {
 	protected AbsolutePanel panel;
 	protected TreeMap<Text, InlineLabel> texts;
-
+	protected static TreeMap<String, Integer[]> fontDimensionsPerFamily = new TreeMap<String, Integer[]>();
+	protected static final int DEFAULT_PADDING_X = 0;
+	protected static final int DEFAULT_PADDING_Y = 3;
+	protected static final Text defaultTextToMeasure = new Text(0, 0, "1234567890", "", 16, "normal", "normal", BasePointXPosition.LEFT, BasePointYPosition.TOP);
+	
 	public TextPositioner(AbsolutePanel panel) {
 		this.panel = panel;
 		this.texts = new TreeMap<Text, InlineLabel>();
@@ -21,8 +29,12 @@ public class TextPositioner {
 	 * @param text
 	 */
 	public void lazyAddText(Text text){
-		if(text != null)
+		if(text != null){
 			texts.put(text, null);
+			if(text.getFontFamily()!= null && text.getFontFamily().length() > 0 && !fontDimensionsPerFamily.containsKey(text.getFontFamily())){
+				fontDimensionsPerFamily.put(text.getFontFamily(), null);
+			}
+		}
 	}
 	
 	/**
@@ -30,11 +42,102 @@ public class TextPositioner {
 	 * @param text
 	 */
 	public void addText(Text text){
-		if(text != null){
-			InlineLabel lbl = createLabel(text);
-			panel.add(lbl, text.getBasePointX(), text.getBasePointY());
-			texts.put(text, lbl);
+		if(text != null && text.getText().length() > 0){
+//			displayText(text);
+			displayTextWithMeasure(text);
 		}
+	}
+	
+	protected void displayTextWithMeasure(Text text){
+		if(texts.get(text) != null){
+			panel.remove(texts.get(text));
+		}
+		if(text.getBasePointXPosition() != BasePointXPosition.LEFT || text.getBasePointYPosition() != BasePointYPosition.TOP){
+			InlineLabel lbl = createLabel(text);
+			lbl.getElement().getStyle().setPadding(0, Unit.PX);
+			lbl.getElement().getStyle().setMargin(0, Unit.PX);
+			RootPanel.get().add(lbl);
+			text.setWidth(lbl.getOffsetWidth());
+			text.setHeight(lbl.getOffsetHeight() + DEFAULT_PADDING_Y);
+			RootPanel.get().remove(lbl);
+		}
+		
+		int x = 1,y = 1;
+		switch(text.getBasePointXPosition()){
+		case LEFT:
+			x = text.getBasePointX() + text.getLeftPadding();
+			break;
+		case MIDDLE:
+			x = text.getBasePointX() - (text.getWidth() + text.getLeftPadding() + text.getRightPadding())/2;
+			break;
+		case RIGHT: 
+			x = text.getBasePointX() - (text.getWidth() + text.getLeftPadding() + text.getRightPadding());
+			break;
+		}
+		switch(text.getBasePointYPosition()){
+		case TOP:
+			y = text.getBasePointY() + text.getTopPadding();
+			break;
+		case MIDDLE:
+			y = text.getBasePointY() - (text.getHeight() + text.getTopPadding() + text.getBottomPadding())/2;
+			break;
+		case BOTTOM: 
+			y = text.getBasePointY() - (text.getHeight()  + text.getTopPadding() + text.getBottomPadding());
+			break;
+		}
+		InlineLabel lbl = createLabel(text);
+		panel.add(lbl, x, y);
+		texts.put(text, lbl);
+	}
+	
+	protected void displayText(Text text){
+		if(text.getWidth() == 0 || text.getHeight() == 0)
+			updateTextDimensions(text, true);
+		if(texts.get(text) != null){
+			panel.remove(texts.get(text));
+		}
+		int x = 1,y = 1;
+		switch(text.getBasePointXPosition()){
+		case LEFT:
+			x = text.getBasePointX() + text.getLeftPadding();
+			break;
+		case MIDDLE:
+			x = text.getBasePointX() - (text.getWidth() + text.getLeftPadding() + text.getRightPadding())/2;
+			break;
+		case RIGHT: 
+			x = text.getBasePointX() - (text.getWidth() + text.getLeftPadding() + text.getRightPadding());
+			break;
+		}
+		switch(text.getBasePointYPosition()){
+		case TOP:
+			y = text.getBasePointY() + text.getTopPadding();
+			break;
+		case MIDDLE:
+			y = text.getBasePointY() - (text.getHeight() + text.getTopPadding() + text.getBottomPadding())/2;
+			break;
+		case BOTTOM: 
+			y = text.getBasePointY() - (text.getHeight()  + text.getTopPadding() + text.getBottomPadding());
+			break;
+		}
+		InlineLabel lbl = createLabel(text);
+		panel.add(lbl, x, y);
+		texts.put(text, lbl);
+	}
+	
+	protected boolean updateTextDimensions(Text text, boolean measureIfFontFamilyNotPresent){
+		if(measureIfFontFamilyNotPresent && (!fontDimensionsPerFamily.containsKey(text.getFontFamily()) || fontDimensionsPerFamily.get(text.getFontFamily()) == null))
+			measureFontFamily(text.getFontFamily());
+		if(fontDimensionsPerFamily.containsKey(text.getFontFamily())){
+			Integer[] ffd = fontDimensionsPerFamily.get(text.getFontFamily());
+			if(ffd == null)
+				return false;
+			text.setWidth( (int) ((((ffd[0]-DEFAULT_PADDING_X) / 10d) / 16d) * text.getFontSize() * text.getText().length()));
+//			text.setHeight((ffd[1]-DEFAULT_PADDING_Y) / 16 * text.getFontSize() + DEFAULT_PADDING_Y);
+			text.setHeight((int) ((ffd[1]-DEFAULT_PADDING_Y) / 16d * text.getFontSize() + DEFAULT_PADDING_Y));
+			return true;
+		}
+		else
+			return false;
 	}
 	
 	public void removeAllText(){
@@ -52,20 +155,42 @@ public class TextPositioner {
 		texts.remove(text);
 	}
 	
-	public void update(){
-		for(Text text : texts.keySet()){
-			if(texts.get(text) != null){
-				panel.remove(texts.get(text));
+	protected void updateFontFamilyDimensions(boolean measureAll){
+		for(String ff : fontDimensionsPerFamily.keySet()){
+			if(measureAll || fontDimensionsPerFamily.get(ff) == null){
+				measureFontFamily(ff);
 			}
-			InlineLabel lbl = createLabel(text);
-			panel.add(lbl, text.getBasePointX(), text.getBasePointY());
-			texts.put(text, lbl);
+		}
+	}
+	
+	protected void measureFontFamily(String ff){
+		if(ff != null && ff.length() > 0){
+			defaultTextToMeasure.setFontFamily(ff);
+			InlineLabel testLabel = createLabel(defaultTextToMeasure);
+			testLabel.getElement().getStyle().setPadding(0, Unit.PX);
+			testLabel.getElement().getStyle().setMargin(0, Unit.PX);
+			RootPanel.get().add(testLabel);
+			Integer[] dimensions = new Integer[]{testLabel.getOffsetWidth(), testLabel.getOffsetHeight()}; 
+			RootPanel.get().remove(testLabel);
+			fontDimensionsPerFamily.put(ff, dimensions);
+		}
+	}
+	
+	public void update(){
+		updateFontFamilyDimensions(false);
+		for(Text text : texts.keySet()){
+//			displayText(text);
+			displayTextWithMeasure(text);
 		}
 	}
 	
 	private static InlineLabel createLabel(Text label){
 		InlineLabel lbl = new InlineLabel(label.getText());
-		//TODO
+		lbl.getElement().getStyle().setFontSize(label.getFontSize(), Unit.PX);
+		lbl.getElement().setAttribute("fontFamily", label.getFontFamily());
+		lbl.getElement().setAttribute("color", label.getColor());
+		lbl.getElement().setAttribute("fontStyle", label.getFontStyle());
+		lbl.getElement().setAttribute("fontWeight", label.getFontWeight());
 		return lbl;
 	}
 	
--- inegraphics/src/main/java/com/inepex/inegraphics/shared/gobjects/Text.java
+++ inegraphics/src/main/java/com/inepex/inegraphics/shared/gobjects/Text.java
@@ -3,24 +3,79 @@ package com.inepex.inegraphics.shared.gobjects;
 import com.inepex.inegraphics.shared.Context;
 
 public class Text extends GraphicalObject implements Comparable<Text>{
-	
-	//TODO font style, size, etc
-	protected String text;
+	//comparing helper fields
 	private static int highestComparatorID = Integer.MIN_VALUE;
 	private int comparatorID;
+	//TODO font style, size, etc
+	protected String text;
+	protected String fontFamily;
+	protected int fontSize;
+	protected String fontStyle;
+	protected String fontWeight;
+	protected int width = 0;
+	protected int height = 0;
+	protected BasePointXPosition basePointXPosition;
+	protected BasePointYPosition basePointYPosition;
+	protected int[] padding = new int[]{0,0,0,0};
+	/**
+	 * 
+	 * Vertical Positioning
+	 */
+	public enum BasePointYPosition{
+		TOP,
+		MIDDLE,
+		BOTTOM
+	}
+	/**
+	 * 
+	 * Horizontal Positioning
+	 */
+	public enum BasePointXPosition{
+		LEFT,
+		MIDDLE,
+		RIGHT
+	}
+	//font color is stored in context color
+	protected final static Context DEFAULT_FONT_CONTEXT = new Context(1, "#ffffff", 1, "#ffffff", 0, 0, 0,  "#ffffff");
+	protected final static String DEFAULT_FONT_FAMILY = "Calibri, Verdana, Arial, sans-serif";
+	protected final static int DEFAULT_FONT_SIZE = 11;
+	protected final static String DEFAULT_FONT_STYLE = "normal";
+	protected final static String DEFAULT_FONT_WEIGHT = "normal";
 	
 	public Text(String text,int basePointX, int basePointY){
-		this(text, basePointX, basePointY, 100000, Context.getDefaultContext(), false, false);
+		this(basePointX, basePointY, text, DEFAULT_FONT_FAMILY, DEFAULT_FONT_SIZE, DEFAULT_FONT_STYLE, DEFAULT_FONT_WEIGHT, BasePointXPosition.LEFT, BasePointYPosition.TOP);
 	}
 	
-	protected Text(String text,int basePointX, int basePointY, int zIndex, Context context,
-			boolean stroke, boolean fill) {
-		super(basePointX, basePointY, zIndex, context, stroke, fill);
-		comparatorID = highestComparatorID++;
-		this.text = text;
+	
+	public Text(String text,int basePointX, int basePointY, BasePointXPosition basePointXPosition,	BasePointYPosition basePointYPosition){
+		this(basePointX, basePointY, text, DEFAULT_FONT_FAMILY, DEFAULT_FONT_SIZE, DEFAULT_FONT_STYLE, DEFAULT_FONT_WEIGHT, basePointXPosition, basePointYPosition);
 	}
 
-	
+	/**
+	 * @param basePointX
+	 * @param basePointY
+	 * @param text
+	 * @param fontFamily CSS
+	 * @param fontSize CSS
+	 * @param fontStyle CSS
+	 * @param fontWeight CSS
+	 * @param basePointXPosition {@link BasePointXPosition}
+	 * @param basePointYPosition {@link BasePointYPosition}
+	 */
+	public Text(int basePointX, int basePointY, String text,
+			String fontFamily,int fontSize, String fontStyle, String fontWeight, 
+			BasePointXPosition basePointXPosition,	BasePointYPosition basePointYPosition) {
+		super(basePointX, basePointY, 0, DEFAULT_FONT_CONTEXT, false, false);
+		this.comparatorID = highestComparatorID++;
+		this.text = text;
+		this.fontFamily = fontFamily;
+		this.fontSize = fontSize;
+		this.fontStyle = fontStyle;
+		this.fontWeight = fontWeight;
+		this.basePointXPosition = basePointXPosition;
+		this.basePointYPosition = basePointYPosition;
+	}
+
 	/**
 	 * @return the text
 	 */
@@ -28,6 +83,7 @@ public class Text extends GraphicalObject implements Comparable<Text>{
 		return text;
 	}
 
+	
 	/**
 	 * @param text the text to set
 	 */
@@ -35,9 +91,195 @@ public class Text extends GraphicalObject implements Comparable<Text>{
 		this.text = text;
 	}
 
+	
 	@Override
 	public int compareTo(Text other) {
 		return comparatorID - other.comparatorID;
+	}
+
+
+	/**
+	 * @return the fontFamily
+	 */
+	public String getFontFamily() {
+		return fontFamily;
+	}
+
+
+	/**
+	 * @param fontFamily the fontFamily to set
+	 */
+	public void setFontFamily(String fontFamily) {
+		this.fontFamily = fontFamily;
+	}
+
+
+	/**
+	 * @return the fontSize
+	 */
+	public int getFontSize() {
+		return fontSize;
+	}
+
+
+	/**
+	 * @param fontSize the fontSize to set
+	 */
+	public void setFontSize(int fontSize) {
+		this.fontSize = fontSize;
+	}
+
+
+	/**
+	 * @return the fontStyle
+	 */
+	public String getFontStyle() {
+		return fontStyle;
+	}
+
+
+	/**
+	 * @param fontStyle the fontStyle to set
+	 */
+	public void setFontStyle(String fontStyle) {
+		this.fontStyle = fontStyle;
+	}
+
+
+	/**
+	 * @return the fontWeight
+	 */
+	public String getFontWeight() {
+		return fontWeight;
+	}
+
+
+	/**
+	 * @param fontWeight the fontWeight to set
+	 */
+	public void setFontWeight(String fontWeight) {
+		this.fontWeight = fontWeight;
+	}
+
+
+	/**
+	 * The height of the displayed text.
+	 * @return the width 0 if it has not been displayed yet
+	 */
+	public int getWidth() {
+		return width;
+	}
+
+
+	/**
+	 * Do NOT use this method.
+	 * The text positioner uses it to properly position the text.
+	 * @param width the width to set
+	 */
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+
+	/**
+	 * The height of the displayed text.
+	 * @return the height 0 if it has not been displayed yet
+	 */
+	public int getHeight() {
+		return height;
+	}
+
+
+	/**
+	 * Do NOT use this method.
+	 * The text positioner modul uses it to properly position the text.
+	 * @param height the height to set
+	 */
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+
+	/**
+	 * @return the basePointXPosition
+	 */
+	public BasePointXPosition getBasePointXPosition() {
+		return basePointXPosition;
+	}
+
+
+	/**
+	 * @param basePointXPosition the {@link BasePointXPosition} to set
+	 */
+	public void setBasePointXPosition(BasePointXPosition basePointXPosition) {
+		this.basePointXPosition = basePointXPosition;
+	}
+
+
+	/**
+	 * @return the basePointYPosition
+	 */
+	public BasePointYPosition getBasePointYPosition() {
+		return basePointYPosition;
+	}
+
+
+	/**
+	 * @param basePointYPosition the {@link BasePointYPosition} to set
+	 */
+	public void setBasePointYPosition(BasePointYPosition basePointYPosition) {
+		this.basePointYPosition = basePointYPosition;
+	}
+	
+	
+	public String getColor(){
+		return context.getFillColor();
+	}
+	
+	
+	public void setColor(String color){
+		context.setFillColor(color);
+		context.setStrokeColor(color);
+		context.setShadowColor(color);
+	}
+	
+	public void setPadding(int top, int right, int bottom, int left){
+		this.padding[0] = top;
+		this.padding[1] = right;
+		this.padding[2] = bottom;
+		this.padding[3] = left;
+	}
+	
+	public void setTopPadding(int top){
+		this.padding[0] = top;
+	}
+	
+	public void setRightPadding(int right){
+		this.padding[1] = right;
+	}
+	
+	public void setBottomPadding(int bottom){
+		this.padding[2] = bottom;
+	}
+	
+	public void setLeftPadding(int left){
+		this.padding[3] = left;
+	}
+	
+	public int getTopPadding(){
+		return this.padding[0];
+	}
+	
+	public int getRightPadding(){
+		return this.padding[1];
+	}
+	
+	public int getBottomPadding(){
+		return this.padding[2] ;
+	}
+	
+	public int getLeftPadding(){
+		return this.padding[3];
 	}
 
 }
