9542fb5e566f73c7a815d09259bd346529f0d62d
miklos.suveges <miklos.suveges@inepex.com>
2011-07-11 17:14:26 +0000

    designed colorset added to defaults.
    git-svn-id: http://inechart.googlecode.com/svn/trunk@228 8b7161a9-059b-3e57-6d5a-16b2b4bc90f4

--- inechart/src/main/java/com/inepex/inechart/chartwidget/misc/ColorSet.java
+++ inechart/src/main/java/com/inepex/inechart/chartwidget/misc/ColorSet.java
@@ -4,15 +4,30 @@ import com.inepex.inechart.chartwidget.properties.Color;
 
 public class ColorSet {
 
-	static final Color[] defaultColors = { new Color("#edc240"),
+	static final Color[] flotColorSet = { new Color("#edc240"),
 			new Color("#afd8f8"), new Color("#cb4b4b"), new Color("#4da74d"),
 			new Color("#9440ed"), new Color("#9440ed"), new Color("#9440ed"),
 			new Color("#9440ed") };
+	
+	static final Color[] ineColorSet = {
+		new Color("#1c91db"),
+		new Color("#dfd212"),
+		new Color("#de8238"),
+		new Color("#e4292d"),
+		new Color("#12e8de"),
+		new Color("#b6c317"),
+		new Color("#f5a032"),
+		new Color("#fb3f21"),
+		new Color("#0fcbf2"),
+		new Color("#d5b914"),
+		new Color("#eba626"),
+		new Color("#f15b1f")
+	};
 	Color[] colors;
 	int actual = 0;
 
 	public ColorSet() {
-		this(defaultColors);
+		this(ineColorSet);
 	}
 
 	public ColorSet(Color... colors) {
