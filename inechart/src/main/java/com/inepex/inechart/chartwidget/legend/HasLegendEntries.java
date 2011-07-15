d2fc19bd0d0e38c00fb1ca87a1337a81e40c48eb
miklos.suveges <miklos.suveges@inepex.com>
2011-07-15 17:26:32 +0000

    Synchronizing, previous commit fail
    git-svn-id: http://inechart.googlecode.com/svn/trunk@245 8b7161a9-059b-3e57-6d5a-16b2b4bc90f4

--- inechart/src/main/java/com/inepex/inechart/chartwidget/legend/HasLegendEntries.java
+++
@@ -1,9 +0,0 @@
-package com.inepex.inechart.chartwidget.legend;
-
-import java.util.List;
-
-public interface HasLegendEntries {
-	List<LegendEntry> getEntries();
-	boolean showLegend();
-	void setShowLegend(boolean showLegend);
-}
