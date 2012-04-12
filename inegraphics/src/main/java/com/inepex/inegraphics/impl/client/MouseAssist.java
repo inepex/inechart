c0773276750c568e7a690ec413cd94c07864b3ae
miklos.suveges <miklos.suveges@inepex.com>
2012-04-12 17:48:19 +0000

    go mouse over binarysearch
    git-svn-id: http://inechart.googlecode.com/svn/trunk@366 8b7161a9-059b-3e57-6d5a-16b2b4bc90f4

--- inegraphics/src/main/java/com/inepex/inegraphics/impl/client/MouseAssist.java
+++ inegraphics/src/main/java/com/inepex/inegraphics/impl/client/MouseAssist.java
@@ -1,5 +1,8 @@
 package com.inepex.inegraphics.impl.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.inepex.inegraphics.shared.DrawingAreaAssist;
 import com.inepex.inegraphics.shared.gobjects.Circle;
 import com.inepex.inegraphics.shared.gobjects.GraphicalObject;
@@ -8,8 +11,106 @@ import com.inepex.inegraphics.shared.gobjects.PathElement;
 import com.inepex.inegraphics.shared.gobjects.Rectangle;
 
 public class MouseAssist {
-
-
+	
+	/**
+	 * GOs must have same size to make it work properly
+	 * The given collection will be sorted by canvasX
+	 * @param coords
+	 * @param gos circles or rectangles
+	 * @return null if not circles or rectangles
+	 */
+	public static List<GraphicalObject> getMouseOverGOs(int[] coords, List<GraphicalObject> gos) {
+		if(gos.size() == 0){
+			return null;
+		}
+		double from, to;
+		GraphicalObject first = gos.get(0);
+		if(first instanceof Circle){
+			from = coords[0] - ((Circle)first).getRadius() + first.getContext().getStrokeWidth() / 2;
+			to = coords[0] + ((Circle)first).getRadius() + first.getContext().getStrokeWidth() / 2;
+		}
+		else if (first instanceof Rectangle){
+			from = coords[0] + first.getContext().getStrokeWidth() / 2;
+			to = coords[0] + ((Rectangle)first).getWidth() + first.getContext().getStrokeWidth() / 2;
+		}
+		else{
+			return null;
+		}
+		List<GraphicalObject> subList = getSublist(gos, from, to);
+		List<GraphicalObject> over = new ArrayList<GraphicalObject>();
+		for(GraphicalObject go : subList){
+			if(isMouseOver(coords, go)){
+				over.add(go);
+			}
+		}
+		return over;
+	}
+	
+	public static List<GraphicalObject> getSublist(List<GraphicalObject> gos, double fromX, double toX){
+		int i = binarySearchGOByBasePointX(fromX, gos, GraphicalObjectSearchParameter.higher);
+		int j = binarySearchGOByBasePointX(toX, gos, GraphicalObjectSearchParameter.lower);
+		if(i == -1){
+			i = 0;
+		}
+		if(j == -1){
+			j = gos.size() - 1;
+		}
+		return gos.subList(i, j);
+	}
+	
+	public static enum GraphicalObjectSearchParameter{
+		exact,
+		lower,
+		higher,
+		closest
+	}
+	
+	public static int binarySearchGOByBasePointX(double basePointX, List<GraphicalObject> gos,  GraphicalObjectSearchParameter gosp){
+		int intervalLeft = 0;
+		int intervalRight = gos.size() - 1;
+		int intervalMid = 0;
+		double midBPX = 0;
+		while (intervalRight >= intervalLeft){
+			intervalMid = (intervalLeft + intervalRight) / 2;
+			midBPX = gos.get(intervalMid).getBasePointX();
+			if (midBPX < basePointX){
+				intervalLeft = intervalMid + 1;
+			}
+			else if (midBPX > basePointX){
+				intervalRight = intervalMid - 1;
+			}
+			else{ //midvalue = x;
+				return intervalMid;
+			}
+		}
+		if(gosp == GraphicalObjectSearchParameter.exact){
+			return -1;
+		}
+		else{
+			int lowerIndex;
+			int higherIndex;
+			if(midBPX > basePointX){
+				lowerIndex = intervalMid - 1;
+				higherIndex = intervalMid;
+			}
+			else{
+				lowerIndex = intervalMid;
+				higherIndex = intervalMid + 1;
+			}
+			if(gosp == GraphicalObjectSearchParameter.higher){
+				return higherIndex < gos.size() ? higherIndex : - 1;
+			}
+			else if(gosp == GraphicalObjectSearchParameter.lower){
+				return lowerIndex >= 0 ? lowerIndex : -1;
+			}
+			else { //closest
+				Double lowerValue = lowerIndex >= 0 ? gos.get(lowerIndex).getBasePointX() : Double.NEGATIVE_INFINITY;
+				Double higherValue = higherIndex < gos.size()  ? gos.get(higherIndex).getBasePointX() : Double.POSITIVE_INFINITY;
+				return Math.abs(lowerValue - basePointX) < Math.abs(higherValue - basePointX) ? lowerIndex : higherIndex;
+			}
+		}
+	}
+	
 	public static boolean isMouseOver(int[] position, GraphicalObject go){
 		if(go instanceof Path){
 			return isMouseOver(position, (Path)go);
@@ -26,7 +127,6 @@ public class MouseAssist {
 
 	public static boolean isMouseOver(int[] position, GraphicalObject go, int distance){
 		return false;
-
 	}
 
 	public static boolean isMouseOver(int[] position, Circle go){
