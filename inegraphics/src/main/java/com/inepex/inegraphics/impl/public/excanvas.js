c3fa78d1ecfbdd67a9ba6553ffde41f336855506
miklos.suveges <miklos.suveges@inepex.com>
2012-08-13 09:37:15 +0000

    fix for ie8
    git-svn-id: http://inechart.googlecode.com/svn/trunk@379 8b7161a9-059b-3e57-6d5a-16b2b4bc90f4

--- inegraphics/src/main/java/com/inepex/inegraphics/impl/public/excanvas.js
+++ inegraphics/src/main/java/com/inepex/inegraphics/impl/public/excanvas.js
@@ -588,7 +588,9 @@ if (!document.createElement('canvas').getContext) {
     this.canvas = canvasElement;
 
     var cssText = 'width:' + canvasElement.clientWidth + 'px;height:' +
-        canvasElement.clientHeight + 'px;overflow:hidden;position:absolute';
+        canvasElement.clientHeight + 'px;'+
+//        overflow:hidden;
+    +'position:absolute';
     var el = canvasElement.ownerDocument.createElement('div');
     el.style.cssText = cssText;
     canvasElement.appendChild(el);
